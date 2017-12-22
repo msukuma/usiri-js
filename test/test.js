306f32c457cac630649043a7e8ee336c3de99797
Itenga <itenga@protonmail.com>
2017-12-22 22:36:32 +0300

    testing

--- test/test.js
+++ test/test.js
@@ -1,34 +1,81 @@
-import makePassword from '../lib/usiri'
-import getConstants from '../lib/constants/constants'
-import { exec, execSync, spawn } from 'child_process'
-import path from 'path'
-import assert from 'assert'
-
-const locale = 'sw'
-const constants = getConstants(locale)
-const test = 'test'
-
+const assert = require('assert');
+const constants = require('../lib/constants/en');
+const isSymbol = require('../lib/helpers').isSymbol;
+const Usiri = require('../lib/usiri');
+let usiri;
 const args = {
-  name: test,
-  site: test,
-  masterPassword: test,
-  length:  constants.defaults.length,
+  name: constants.test,
+  site: constants.test,
+  masterPassword: constants.test,
   symbols: constants.defaults.symbols,
+  length:  constants.defaults.length,
   release: constants.defaults.release,
-  locale: locale,
-}
+};
 
-const binPath = path.resolve(__dirname, '..', '..', 'usiri', 'bin', 'usiri')
-const cmd = `${binPath} -j ${test} -s ${test} -p ${test} -a ${args.symbols} -u ${args.length} -t ${args.release}`;
+console.log(args);
 
-const usiriRb = exec(cmd, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  const pw = execSync('pbpaste').toString();
-  const password = makePassword(args)
-  assert.equal(pw, password)
-  // console.log(assert)
-  // console.log(assert.equal)
-})
+describe('Usiri', () => {
+  describe('class definition', ()=> {
+    it('should be a class', () => {
+      assert(usiri.constructor === Usiri);
+    });
+
+    it('should accept a plain object with locale defined', () => {
+      assert(usiri.constructor === Usiri);
+    });
+  });
+
+  describe('#makeId', () => {
+    it('should return a string id', () => {
+      const expected = Object.values(args).reduce((a, b) => a + b);
+      const got = usiri.makeId(args);
+      assert.equal(expected, got);
+    });
+  });
+
+  describe('#replaceSymbols', () => {
+    const password = '"';
+    let got = usiri.replaceSymbols(password, constants.symbols.none);
+
+    it('should return password with same length', () => {
+      assert(password.length === got.length);
+    });
+
+    it('should replace symbols with at least one from symbols provided', () => {
+      assert(password !== got);
+      assert(!isSymbol(got));
+
+      got = usiri.replaceSymbols(password, constants.symbols.unambiguous);
+
+      assert(password !== got);
+      assert(isSymbol(got));
+    });
+  });
+
+  describe('#insertSymbol', () => {
+    const password = 'p';
+    let got = usiri.insertSymbol(password, constants.symbols.none);
+
+    it('should return password with same length', () => {
+      assert(password.length === got.length);
+    });
+
+    it('should insert a symbols from symbols provided', () => {
+      assert(password !== got);
+      assert(!isSymbol(got));
+
+      got = usiri.insertSymbol(password, constants.symbols.unambiguous);
+
+      assert(password !== got);
+      assert(isSymbol(got));
+    });
+  });
+
+  describe('#makePassword', () => {
+    let password = usiri.makePassword(args);
+
+    it('should return a password string based on inputs', () => {
+      assert(typeof password === 'string');
+    });
+  });
+});
